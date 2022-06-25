cca2b2d556b632ac55bfab174e7ca8ab22dc1f8a
chlucas324 <chlucas324@gmail.com>
2022-06-25 12:37:25 -0400

    update thought schema

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true 
+        },
+        id: false
+    }
+);
+
+// retrieves the length of the thought's reactions array field on query.
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -44,3 +44,5 @@ UserSchema.virtual('friendCount').get(function() {
 });
 
 const User = model('User', UserSchema);
+
+module.exports = User;
