f7fe004bd2940f3cdc14b77bd6b37ae52cbb780b
chlucas324 <chlucas324@gmail.com>
2022-06-25 18:11:22 -0400

    update user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,8 +1,22 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 const {
-    getAllUsers,
-    getUserById,
-    createUser,
-    
-}
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/user-controller");
+
+//set up GET all and POST at /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+//set up GET one, PUT, DELETE at /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+//add and delete friend
+router.route("/:id/friends/:friendsId").post(addFriend).delete(removeFriend);
+
+module.exports = router;
